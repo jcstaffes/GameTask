1c416d3956ef5ac8c831126d84ff886e90df1b54
u6718946 <u6718946@anu.edu.au>
2019-03-26 11:47:19 +1100

    a littl change of Board.class

--- src/comp1110/ass2/Board.java
+++ src/comp1110/ass2/Board.java
@@ -1,13 +1,19 @@
 package comp1110.ass2;
 
 public class Board {
+    public String emptyboard(){
+
+    }
     public static int emptyBpard(int n){
         return '0';
     }
+
     /***  use this class to complete three steps:
      *    1. create an empty board
      *    2. define the exit
      *    3. define the central area
      *    then the game.java and railroadInk.java can invoke the functions or value in this class.
      **/
+
+    public String exit;
 }
